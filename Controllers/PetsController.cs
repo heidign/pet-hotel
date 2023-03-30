47bf89279e738cc8dc4e8f3236e1e07df0feb05e
Heidi Ganac Nicholson <heidiganac@gmail.com>
2023-03-30 13:13:01 -0500

    PUT by id

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -15,39 +15,101 @@ namespace pet_hotel.Controllers
     public class PetsController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetsController(ApplicationContext context) {
+        public PetsController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<Pet> GetPets() {
+        public IEnumerable<Pet> GetPets()
+        {
             return new List<Pet>();
         }
 
         // [HttpGet]
-        // [Route("test")]
-        // public IEnumerable<Pet> GetPets() {
-        //     PetOwner blaine = new PetOwner{
+        // [Route("api/[controller]")]
+        // public IEnumerable<Pet> GetPets()
+        // {
+        //     PetOwner blaine = new PetOwner
+        //     {
         //         name = "Blaine"
         //     };
 
-        //     Pet newPet1 = new Pet {
+        //     Pet newPet1 = new Pet
+        //     {
         //         name = "Big Dog",
         //         petOwner = blaine,
         //         color = PetColorType.Black,
         //         breed = PetBreedType.Poodle,
         //     };
 
-        //     Pet newPet2 = new Pet {
+        //     Pet newPet2 = new Pet
+        //     {
         //         name = "Little Dog",
         //         petOwner = blaine,
         //         color = PetColorType.Golden,
         //         breed = PetBreedType.Labrador,
         //     };
 
-        //     return new List<Pet>{ newPet1, newPet2};
+        //     return new List<Pet> { newPet1, newPet2 };
         // }
+
+        // * Put by id * //
+        [HttpPut("{id}")]
+        public IActionResult UpdatePet(int id, Pet pet)
+        {
+            pet.id = id;
+
+            // update db context with pet id
+            _context.Update(pet);
+
+            //  save changes to object in db
+            _context.SaveChanges();
+
+            //  return pet object
+            return Ok(pet);
+        }
+
+        // * Put check in * //
+        [HttpPut("{id}/checkin")]
+
+        public IActionResult PutCheckIn(int id)
+        {
+            Console.WriteLine("test");
+
+            Pet pet = _context.Pets.SingleOrDefault(pet => pet.id == id);
+            
+            pet.checkedInAt = DateTime.Now;
+   
+            _context.Update(pet);
+
+            // save changes to pet object in db
+            _context.SaveChanges();
+
+            // return response from pet object 
+            return Ok(pet);
+        }
+
+        // * Put check out * //
+        [HttpPut("{id}/checkout")]
+        public IActionResult PutCheckOut(int id)
+        {
+            Console.WriteLine("test");
+        
+            Pet pet = _context.Pets.Find(id);
+
+            pet.checkedInAt = null;
+
+            _context.Update(pet);
+
+            // save changes to db
+            _context.SaveChanges();
+
+            return Ok(pet);
+        }
+
+
     }
 }
